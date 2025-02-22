7b3d98acb10ade61b323e3c2164d40674be2a3c3
ma.ring <melo.69mane@gmail.com>
2025-02-22 16:47:45 +0900

    add project

---
+++ wave/wave.js
@@ -0,0 +1,26 @@
+class wave{
+    constructor(csize){
+        this.y = random(csize);
+        this.amp = random(csize*0.1,csize);
+        this.rad = random(-180,180);
+        this.csize = csize;
+        this.color = [random(255),random(255),random(255),100];
+        this.speed = random(-1.5,1.5);
+        this.start = random(20);
+    }
+
+    draw(){
+        var sp = this.speed*frameCount*0.5*sin(frameCount*0.05);
+
+        stroke(this.color,100);
+        for(var x =this.start*sin(frameCount); x < this.csize;x+=5*(1+0.5*cos(frameCount))){
+            var px = 10*sin(this.rad +sp+x)*tan(sp);
+            var py = sin(sp)*300*cos(sp+x+ this.rad )*sin(sp);
+            var dy = this.y+this.amp*sin(this.rad+x+sp);
+            //line(x,dy+this.csize*cos(frameCount)+this.csize,x,dy);
+            line(px + x,py,
+                 px + x*tan(sp)*sin(sp+x),py + dy*cos(sp+x));
+
+        }
+    }
+}
